44f9d22a2b1b5c3444b1bcb291bd75b562a21ee6
senir <304875853@qq.com>
2019-04-24 21:52:15 +0800

    :sparkles: 迭代

--- index.tsx
+++ index.tsx
@@ -5,8 +5,9 @@ import get from 'lodash/get';
 import Form from './form';
 
 interface FormProviderProps {
-  value: object;
-  children: React.children;
+  initialValue: object;
+  submit: () => void;
+  children: React.ReactNode;
 }
 
 interface Rule {
@@ -22,13 +23,48 @@ interface DecodeOption {
 
 let cache = {};
 
-function createFormProvider() {
+function createFormProvider(createOptions) {
   const cache = {};
   const [ formData, setFormData ] = React.useState({});
   const FormProvider: React.FC<FormProviderProps> = ({ initialValue, submit, children }) => {
     setFormData(initialValue);
     return <div>{children}</div>
   }
+
+  const fieldsOptions: {} = {} as any;
+
+  const getFieldProps = (
+    name: string,
+    options: DecodeOption,
+  ) => ({
+    [fieldsOptions[name].valuePropName || 'value']: formData[name],
+    [fieldsOptions[name].trigger || 'onChange']: (e: string | any) => {
+      const value = (e && e.target) ? e.target.value : e;
+      setFormData(oldValues => {
+        const values  = {
+          ...oldValues,
+           [name]: value,
+        };
+
+        cache.currentField = name;
+        cache.fieldsTouched[name] = true;
+        if (createOptions.onValuesChange) {
+            createOptions.onValuesChange({
+              [name]: value,
+            });
+          }
+
+        return values;
+      });
+    },
+    // ['data-__field']: { errors: errors[name] },
+    // ['data-__meta']: {
+    //   validate: [{
+    //     rules: options.rules,
+    //   }],
+    // },
+  });
+
   //  正在考虑是否需要支持property. => observer children form.
   function getFieldDecorator(field: string, options: DecodeOption) {
     return (component) => {
@@ -46,15 +82,27 @@ function createFormProvider() {
         component, {
           ...props,
           value: formData[field],
-          onChange: (value) => {
-            const newData = set(formData, field, value);
-            setFormData(newData)
-          }
+          // onChange: (value) => {
+          //   const newData = set(formData, field, value);
+          //   setFormData(newData)
+          // }
+          onChange: onFieldChange(field),
         },
         children
       )
     }
   }
+
+  //  减少因多次render bind function导致的重复渲染。
+  const onFieldChange = React.useCallback(
+    (field: string) => {
+      return (value) => {
+        const newData = set(formData, field, value);
+        setFormData(newData)
+      }
+    },
+    [],
+  );
 
   const setFieldsValue = ({ ...values }) => setFormData({ ...formData, ...values });
 

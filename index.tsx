14e130fd573e191222f7aad1966a28bbc2831a80
senir <dingxue@zhongan.io>
2019-04-24 18:54:17 +0800

    :sparkles: 支持object和array

--- index.tsx
+++ index.tsx
@@ -1,4 +1,6 @@
 import * as React from 'react';
+import isEqual from 'lodash/isEqual';
+import set from 'lodash/set';
 import Form from './form';
 
 interface FormProviderProps {
@@ -6,31 +8,55 @@ interface FormProviderProps {
   children: React.children;
 }
 
-function createFormProvider() {
-  const FormContext = React.createContext({});
-  const [ formData, setFormData ] = React.useState({});
-  const FormProvider: React.FC<FormProviderProps> = ({ formId, value, submit, children }) => {
-    <FormContext.Provider value={formId}>
-      {children}
-    </FormContext.Provider>
-  }
+interface Rule {
+  type: string;
+  required?: boolean;
+  message?: string;
+}
 
+interface DecodeOption {
+  initialValue?: string;
+  rules?: Array<Rule>[];
+}
+
+let cache = {};
+
+function createFormProvider() {
+  const cache = {};
+  const [ formData, setFormData ] = React.useState({});
+  const FormProvider: React.FC<FormProviderProps> = ({ initialValue, submit, children }) => {
+    setFormData(initialValue);
+    return <div>{children}</div>
+  }
   //  正在考虑是否需要支持property. => observer children form.
-  function observer(component) {
-    const { props, children } = component;
-    return React.cloneElement(
-      component, {
-        ...props,
-        onChange: (value) => {
-          setFormData({ ...formData, ...value })
-        }
-      },
-      children
-    )
+  function getFieldDecorator(field: string, options: DecodeOption) {
+    return (component) => {
+      const { props, children } = component;
+      const { initialValue, rules } = options;
+
+      //  change state and not rerender.
+      if (initialValue) {
+        set(formData, field, initialValue);
+      } else {
+        set(formData, field, undefined);
+      }
+
+      return React.cloneElement(
+        component, {
+          ...props,
+          value: formData[field],
+          onChange: (value) => {
+            const newData = set(formData, field, value);
+            setFormData(newData)
+          }
+        },
+        children
+      )
+    }
   }
 
   return {
-    observer,
+    getFieldDecorator,
     FormProvider
   }
 }
